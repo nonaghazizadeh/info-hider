5e2020191905f4150e58c8ede98799332e6c4d58
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-04-27 13:15:35 +0430

    refactor: clean companies crawler code

--- crawelers/companies_crawler.py
+++ crawelers/companies_crawler.py
@@ -1,48 +1,54 @@
 import json
 import requests
 from bs4 import BeautifulSoup
-import re
 from hazm import *
+from static.enums import Keywords, CrawlUrl, Path
 
-companies = []
-url = "http://ircreative.isti.ir/contents.php?cntid=112"
-url2 = "http://ircreative.isti.ir/contents.php?cntid=853"
-f = requests.get(url)
-soup = BeautifulSoup(f.content, 'html5lib')
-rows = soup.findAll("tbody")[0].findAll('tr')
-for i in range(2, len(rows)):
-    row = rows[i]
-    text = row.findAll('td')[1].text
-    text = text.replace('شرکت', '')
-    text = text.replace('موسسه', '')
-    text = text.replace('سازمان', '')
-    companies.append(text)
-f2 = requests.get(url2)
-soup2 = BeautifulSoup(f2.content, 'html5lib')
-rows2 = soup2.findAll("tbody")[0].findAll('tr')
-for i in range(2, len(rows2)):
-    row = rows2[i]
-    companies.append(row.findAll('td')[1].text)
 
-international_companies = []
-url3 = "https://fa.wikipedia.org/wiki/%D9%81%D9%87%D8%B1%D8%B3%D8%AA_%D8%B4%D8%B1%DA%A9%D8%AA%E2%80%8C%D9%87%D8%A7%DB%8C_%D8%A2%D9%85%D8%B1%DB%8C%DA%A9%D8%A7%DB%8C%DB%8C"
-f3 = requests.get(url3)
-soup3 = BeautifulSoup(f3.content, 'html5lib')
-divs = soup3.findAll('div', {'id': 'mw-content-text'})
-links = divs[0].findAll('ul')[0]
-for i in links.findAll('a'):
-    text = i.getText()
-    text = text.replace('شرکت', '')
-    text = text.replace('موسسه', '')
-    text = text.replace('سازمان', '')
-    international_companies.append(text)
+def remove_pre_words(text):
+    for keyword in Keywords.COMPANY_PRE_KEYWORDS.value:
+        text = text.replace(keyword, '')
+    return text
 
-all_companies = companies + international_companies
-normalized_companies = list()
-normalizer = Normalizer()
-for i in all_companies:
-    normalized_companies.append(normalizer.normalize(i))
 
-file = open('../data/companies.json', 'w')
-json.dump(normalized_companies, file, indent=3)
-file.close()
+def iranian_companies_crawler(url):
+    companies_list = list()
+    f_iranian_companies = requests.get(url)
+    soup_iranian_companies = BeautifulSoup(f_iranian_companies.content, 'html5lib')
+    rows = soup_iranian_companies.findAll("tbody")[0].findAll('tr')
+    for i in range(2, len(rows)):
+        plain_text = remove_pre_words(rows[i].findAll('td')[1].text)
+        companies_list.append(plain_text)
+    return companies_list
+
+
+def other_companies_crawler(url):
+    companies_list = list()
+    f_other_companies = requests.get(url)
+    soup_other_companies = BeautifulSoup(f_other_companies.content, 'html5lib')
+    divs = soup_other_companies.findAll('div', {'id': 'mw-content-text'})
+    links = divs[0].findAll('ul')[0]
+    for i in links.findAll('a'):
+        plain_text = remove_pre_words(i.getText())
+        companies_list.append(plain_text)
+    return companies_list
+
+
+def normalize_companies_names(names_list):
+    normalizer = Normalizer()
+    normalized_names_list = list()
+    for name in names_list:
+        normalized_names_list.append(normalizer.normalize(name))
+    return normalized_names_list
+
+
+def write_to_file(companies_names_list, path):
+    file = open(path, 'w')
+    json.dump(companies_names_list, file)
+    file.close()
+
+
+normalized_companies_names = normalize_companies_names(
+    iranian_companies_crawler(CrawlUrl.IRANIAN_FIRST_COMPANY_URL.value) + iranian_companies_crawler(
+        CrawlUrl.IRANIAN_SECOND_COMPANY_URL.value) + other_companies_crawler(CrawlUrl.OTHERS_COMPANY_URL.value))
+write_to_file(normalized_companies_names, Path.COMPANIES_PATH.value)
